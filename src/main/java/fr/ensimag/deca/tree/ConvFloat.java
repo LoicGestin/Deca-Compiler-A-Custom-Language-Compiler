6dcc4ce2d59f2013bf13ed2ded1b1e243a74d9ed
Florian Abou Edou <Florian.Abou-Edou@grenoble-inp.org>
2023-12-21 12:32:55 +0100

    Modifications diverses Maj du planning, maj de la gestion de projet, implémentation de verifyInst de While.

--- src/main/java/fr/ensimag/deca/tree/ConvFloat.java
+++ src/main/java/fr/ensimag/deca/tree/ConvFloat.java
@@ -1,9 +1,7 @@
 package fr.ensimag.deca.tree;
 
-import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.context.*;
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.context.ClassDefinition;
-import fr.ensimag.deca.context.EnvironmentExp;
 
 /**
  * Conversion of an int into a float. Used for implicit conversions.
@@ -18,8 +16,7 @@ public class ConvFloat extends AbstractUnaryExpr {
 
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
-            ClassDefinition currentClass) {
-        throw new UnsupportedOperationException("not yet implemented");
+            ClassDefinition currentClass) throws ContextualError {
     }
 
 

87538c064e96df4bb069ff54757a05c24d54d957
mevels <sarah.mevel@grenoble-inp.org>
2023-12-21 09:55:09 +0100

    Idée Sans-Objet étape B (mais pas testé)

--- src/main/java/fr/ensimag/deca/tree/BooleanLiteral.java
+++ src/main/java/fr/ensimag/deca/tree/BooleanLiteral.java
@@ -1,10 +1,7 @@
 package fr.ensimag.deca.tree;
 
-import fr.ensimag.deca.context.Type;
+import fr.ensimag.deca.context.*;
 import fr.ensimag.deca.DecacCompiler;
-import fr.ensimag.deca.context.ClassDefinition;
-import fr.ensimag.deca.context.ContextualError;
-import fr.ensimag.deca.context.EnvironmentExp;
 import fr.ensimag.deca.tools.IndentPrintStream;
 import java.io.PrintStream;
 
@@ -28,7 +25,7 @@ public class BooleanLiteral extends AbstractExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        setType(compiler.environmentType.defOfType(compiler.symbolTable.create("boolean")).getType());
+        setType(new BooleanType(compiler.createSymbol("boolean")));
         return getType();
     }
 

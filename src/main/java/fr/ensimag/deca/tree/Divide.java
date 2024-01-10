f47a445406c35edc322cf2e9a75565605d88999a
mevels <sarah.mevel@grenoble-inp.org>
2024-01-10 09:21:41 +0100

    Division Zéro

--- src/main/java/fr/ensimag/deca/tree/Divide.java
+++ src/main/java/fr/ensimag/deca/tree/Divide.java
@@ -3,6 +3,7 @@ package fr.ensimag.deca.tree;
 
 import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ContextualError;
+import fr.ensimag.ima.pseudocode.Label;
 import fr.ensimag.ima.pseudocode.instructions.*;
 
 /**
@@ -19,9 +20,14 @@ public class Divide extends AbstractOpArith {
     public void codeGenArith(DecacCompiler compiler) {
         AbstractExpr LValue = this.getLeftOperand();
         AbstractExpr RValue = this.getRightOperand();
+        Label divByZero = new Label("divByZero");
 
         LValue.codeGenInst(compiler);
         RValue.codeGenInst(compiler);
+
+        compiler.getRegistreLibre();
+        compiler.addInstruction(new CMP(0, compiler.getRegistreLibre()));
+        compiler.addInstruction(new BEQ(divByZero));
 
         int number = compiler.getNextRegistreLibre().getNumber();
         
@@ -32,6 +38,10 @@ public class Divide extends AbstractOpArith {
         else {
             compiler.addInstruction(new DIV(compiler.getRegister(number-1), compiler.getRegister(number-2)));
         }
+
+        compiler.addLabel(divByZero);
+        compiler.addInstruction(new WSTR("Erreur : division par 0"));
+
         compiler.libererRegistre();
         compiler.libererRegistre();
     }

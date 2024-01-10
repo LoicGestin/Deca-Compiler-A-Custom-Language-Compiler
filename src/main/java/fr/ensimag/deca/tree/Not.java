d1312a9e2f1f52eef2d305644ffe2737e2b9b0bb
mevels <sarah.mevel@grenoble-inp.org>
2024-01-10 09:36:02 +0100

    Opérateur Logiques début

--- src/main/java/fr/ensimag/deca/tree/Not.java
+++ src/main/java/fr/ensimag/deca/tree/Not.java
@@ -5,6 +5,12 @@ import fr.ensimag.deca.DecacCompiler;
 import fr.ensimag.deca.context.ClassDefinition;
 import fr.ensimag.deca.context.ContextualError;
 import fr.ensimag.deca.context.EnvironmentExp;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.instructions.BNE;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+import sun.management.spi.PlatformMBeanProvider;
 
 /**
  *
@@ -20,9 +26,34 @@ public class Not extends AbstractUnaryExpr {
     @Override
     public Type verifyExpr(DecacCompiler compiler, EnvironmentExp localEnv,
             ClassDefinition currentClass) throws ContextualError {
-        throw new UnsupportedOperationException("not yet implemented");
+        getOperand().verifyCondition(compiler, localEnv, currentClass);
+        setType(compiler.environmentType.BOOLEAN);
+
+        return getType();
     }
 
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+        Label vrai = new Label("vrai");
+        Label fin = new Label("fin");
+
+        getOperand().codeGenInst(compiler);
+
+        compiler.libererRegistre();
+
+        compiler.addInstruction(new CMP(0, compiler.getRegistreLibre()));
+        compiler.addInstruction(new BNE(vrai));
+
+        compiler.libererRegistre();
+
+        compiler.addInstruction(new LOAD(1, compiler.getRegistreLibre()));
+        compiler.addInstruction(new BRA(fin));
+
+        compiler.addLabel(vrai);
+        compiler.addInstruction(new LOAD(0, compiler.getRegistreLibre()));
+
+        compiler.addLabel(fin);
+    }
 
     @Override
     protected String getOperatorName() {
--- src/main/java/fr/ensimag/deca/tree/NotEquals.java
+++ src/main/java/fr/ensimag/deca/tree/NotEquals.java
@@ -1,6 +1,13 @@
 package fr.ensimag.deca.tree;
 
 
+import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.ima.pseudocode.Label;
+import fr.ensimag.ima.pseudocode.instructions.BNE;
+import fr.ensimag.ima.pseudocode.instructions.BRA;
+import fr.ensimag.ima.pseudocode.instructions.CMP;
+import fr.ensimag.ima.pseudocode.instructions.LOAD;
+
 /**
  *
  * @author gl29
@@ -12,7 +19,31 @@ public class NotEquals extends AbstractOpExactCmp {
         super(leftOperand, rightOperand);
     }
 
+    @Override
+    protected void codeGenInst(DecacCompiler compiler) {
+        Label vrai = new Label("vrai");
+        Label fin = new Label("fin");
 
+        getLeftOperand().codeGenInst(compiler);
+        getRightOperand().codeGenInst(compiler);
+
+        compiler.libererRegistre();
+        compiler.libererRegistre();
+
+        compiler.addInstruction(new CMP(compiler.getNextRegistreLibre(), compiler.getNextRegistreLibre()));
+        compiler.addInstruction(new BNE(vrai));
+
+        compiler.libererRegistre();
+        compiler.libererRegistre();
+
+        compiler.addInstruction(new LOAD(0, compiler.getNextRegistreLibre()));
+        compiler.addInstruction(new BRA(fin));
+
+        compiler.addLabel(vrai);
+        compiler.addInstruction(new LOAD(1, compiler.getNextRegistreLibre()));
+
+        compiler.addLabel(fin);
+    }
     @Override
     protected String getOperatorName() {
         return "!=";

a931163883623219ecf0fc523db17193f2bfab39
mevels <sarah.mevel@grenoble-inp.org>
2024-01-11 15:28:01 +0100

    correction

--- src/main/java/fr/ensimag/deca/tree/Plus.java
+++ src/main/java/fr/ensimag/deca/tree/Plus.java
@@ -22,7 +22,7 @@ public class Plus extends AbstractOpArith {
         RValue.codeGenInst(compiler);
         int number = compiler.getNextRegistreLibre().getNumber();
         compiler.addInstruction(new ADD(compiler.getRegister(number - 1), compiler.getRegister(number - 2)));
-        compiler.libererRegistre(3);
+        compiler.libererRegistre(2);
     }
 
 

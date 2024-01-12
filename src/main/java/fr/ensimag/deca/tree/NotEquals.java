69d6e753f55fee3b87594ebe1d0c64311e055657
mevels <sarah.mevel@grenoble-inp.org>
2024-01-12 13:08:36 +0100

    NotEquals fait

--- src/main/java/fr/ensimag/deca/tree/NotEquals.java
+++ src/main/java/fr/ensimag/deca/tree/NotEquals.java
@@ -2,6 +2,7 @@ package fr.ensimag.deca.tree;
 
 
 import fr.ensimag.deca.DecacCompiler;
+import fr.ensimag.deca.codegen.codeGen;
 import fr.ensimag.ima.pseudocode.Label;
 import fr.ensimag.ima.pseudocode.instructions.BNE;
 import fr.ensimag.ima.pseudocode.instructions.BRA;
@@ -26,20 +27,17 @@ public class NotEquals extends AbstractOpExactCmp {
         getLeftOperand().codeGenInst(compiler);
         getRightOperand().codeGenInst(compiler);
 
-        compiler.libererRegistre(2);
 
-        compiler.addInstruction(new CMP(compiler.getNextRegistreLibre(), compiler.getNextRegistreLibre()));
+        compiler.addInstruction(new CMP(codeGen.getRegistreUtilise(), codeGen.getRegistreUtilise()));
         compiler.addInstruction(new BNE(vrai));
 
-        compiler.libererRegistre(2);
 
-        compiler.addInstruction(new LOAD(0, compiler.getNextRegistreLibre()));
+        compiler.addInstruction(new LOAD(0, codeGen.getRegistreLibre()));
         compiler.addInstruction(new BRA(fin));
 
-        compiler.libererRegistre(1);
 
         compiler.addLabel(vrai);
-        compiler.addInstruction(new LOAD(1, compiler.getNextRegistreLibre()));
+        compiler.addInstruction(new LOAD(1, codeGen.getCurrentRegistreUtilise()));
 
         compiler.addLabel(fin);
     }

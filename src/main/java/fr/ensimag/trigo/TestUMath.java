263ddd67807eca8c181841e6f4ae6ecc6172c74f
Daniel Bannister <daniel.bannister@phelma.grenoble-inp.fr>
2024-01-18 19:14:02 +0100

    TRIGO: valeurs constantes Float.decah; ulp fonctionne; puissance négative. TODO: cos, asin et atan

--- src/main/java/fr/ensimag/trigo/TestUMath.java
+++ src/main/java/fr/ensimag/trigo/TestUMath.java
@@ -2,12 +2,13 @@ package fr.ensimag.trigo;
 
 import java.lang.Math;
 
-public class Test_UMath{
+public class TestUMath{
 
     public static void main(String args[]){
         UMath m = new UMath();
         
-        System.out.println("3.5² = "+m.pow((float)3.5,2) +" " + Math.pow((float)3.5,2));
+        System.out.println("3.5² = "+m.pow((float)3.5,2) +" " + (float)Math.pow((float)3.5,2));
+        System.out.println("5.32^-3 = "+m.pow((float)3.5,-3) +" " + (float)Math.pow((float)3.5,-3));
 
         System.out.println("+inf = " + m.POSITIVE_INFINITY + " " + Float.POSITIVE_INFINITY);
         System.out.println("-inf = " + m.NEGATIVE_INFINITY + " " + Float.NEGATIVE_INFINITY);
@@ -22,8 +23,8 @@ public class Test_UMath{
 
         System.out.println("sin(3.12) = " + m.sin(((float)3.12)) + " " + (float)Math.sin((float)3.12));
         System.out.println("cos(65.3265) = " + m.cos(((float)65.3265)) + " " + (float)Math.cos((float)65.3265));
-        System.out.println("asin(0.36657) = " + m.asin(((float)0.36657)) + " " + (float)Math.asin((float)0.36657));
+        System.out.println("asin(1.0) = " + m.asin(((float)1.0)) + " " + (float)Math.asin((float)1.0));
         System.out.println("atan(32.684) = " + m.atan(((float)32.684)) + " " + (float)Math.atan((float)32.684));
-        //System.out.println("ulp(96.57) = " + m.ulp(96.57) + " " + Math.ulp((float)96.57));
+        System.out.println("ulp(75.61) = " + m.ulp((float)75.61) + " " + Math.ulp((float)75.61));
     }
 }

719c504b7cccff78bd51d73d90253a639b080dc1
Daniel Bannister <daniel.bannister@phelma.grenoble-inp.fr>
2024-01-24 21:41:39 +0100

    TRIGO: Màj

---
+++ src/main/java/fr/ensimag/trigo/ComparaisonDecaJava.java
@@ -0,0 +1,81 @@
+package fr.ensimag.trigo;
+
+import java.lang.Math;
+public class ComparaisonDecaJava {
+
+    public  static void main(String[] args) {
+        float f1 = (float)0.001;
+        float f2 = (float)-12.3456;
+        float f3 = (float)0.5;
+        float f4 = (float)9876543.21;
+        float f5 = (float)-0.000001;
+        float f6 = (float)3.14159;
+        float f7 = (float)-876.54321;
+        float f8 = (float)2.71828;
+        float f9 = (float)-999999.999;
+        float f10 = (float)4.71239;
+
+        System.out.println("Fonction sin");
+        System.out.println("sin("+ f1+ ")="+ Math.sin(f1));
+        System.out.println("sin("+ f2+ ")="+ Math.sin(f2));
+        System.out.println("sin("+ f3+ ")="+ Math.sin(f3));
+        System.out.println("sin("+ f4+ ")="+ Math.sin(f4));
+        System.out.println("sin("+ f5+ ")="+ Math.sin(f5));
+        System.out.println("sin("+ f6+ ")="+ Math.sin(f6));
+        System.out.println("sin("+ f7+ ")="+ Math.sin(f7));
+        System.out.println("sin("+ f8+ ")="+ Math.sin(f8));
+        System.out.println("sin("+ f9+ ")="+ Math.sin(f9));
+        System.out.println("sin("+ f10+ ")="+ Math.sin(f10));
+        System.out.println("---------------------------------------------------------");
+
+        System.out.println("Fonction cos");
+        System.out.println("cos("+ f1+ ")="+ Math.cos(f1));
+        System.out.println("cos("+ f2+ ")="+ Math.cos(f2));
+        System.out.println("cos("+ f3+ ")="+ Math.cos(f3));
+        System.out.println("cos("+ f4+ ")="+ Math.cos(f4));
+        System.out.println("cos("+ f5+ ")="+ Math.cos(f5));
+        System.out.println("cos("+ f6+ ")="+ Math.cos(f6));
+        System.out.println("cos("+ f7+ ")="+ Math.cos(f7));
+        System.out.println("cos("+ f8+ ")="+ Math.cos(f8));
+        System.out.println("cos("+ f9+ ")="+ Math.cos(f9));
+        System.out.println("cos("+ f10+ ")="+ Math.cos(f10));
+        System.out.println("---------------------------------------------------------");
+
+        System.out.println("Fonction atan");
+        System.out.println("atan("+ f1+ ")="+ Math.atan(f1));
+        System.out.println("atan("+ f2+ ")="+ Math.atan(f2));
+        System.out.println("atan("+ f3+ ")="+ Math.atan(f3));
+        System.out.println("atan("+ f4+ ")="+ Math.atan(f4));
+        System.out.println("atan("+ f5+ ")="+ Math.atan(f5));
+        System.out.println("atan("+ f6+ ")="+ Math.atan(f6));
+        System.out.println("atan("+ f7+ ")="+ Math.atan(f7));
+        System.out.println("atan("+ f8+ ")="+ Math.atan(f8));
+        System.out.println("atan("+ f9+ ")="+ Math.atan(f9));
+        System.out.println("atan("+ f10+ ")="+ Math.atan(f10));
+        System.out.println("---------------------------------------------------------");
+
+        f1 = (float)-0.723;
+        f2 = (float)0.456;
+        f3 = (float)0.821;
+        f4 = (float)-0.134;
+        f5 = (float)-0.589;
+        f6 = (float)0.675;
+        f7 = (float)-0.287;
+        f8 = (float)0.932;
+        f9 = (float)-0.045;
+        f10 = (float)0.213;
+
+        System.out.println("Fonction asin");
+        System.out.println("asin("+ f1+ ")="+ Math.asin(f1));
+        System.out.println("asin("+ f2+ ")="+ Math.asin(f2));
+        System.out.println("asin("+ f3+ ")="+ Math.asin(f3));
+        System.out.println("asin("+ f4+ ")="+ Math.asin(f4));
+        System.out.println("asin("+ f5+ ")="+ Math.asin(f5));
+        System.out.println("asin("+ f6+ ")="+ Math.asin(f6));
+        System.out.println("asin("+ f7+ ")="+ Math.asin(f7));
+        System.out.println("asin("+ f8+ ")="+ Math.asin(f8));
+        System.out.println("asin("+ f9+ ")="+ Math.asin(f9));
+        System.out.println("asin("+ f10+ ")="+ Math.asin(f10));
+        System.out.println("---------------------------------------------------------");        
+    }
+}
